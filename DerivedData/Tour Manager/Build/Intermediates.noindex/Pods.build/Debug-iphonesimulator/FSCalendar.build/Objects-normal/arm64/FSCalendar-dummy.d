71c8c6d38f0240608619b9c57e5eaff18ca28b43
Egor Zavrazhnov <88378430+SHREDDING8@users.noreply.github.com>
2023-06-19 15:13:31 +0300

    added excursions for guides

--- DerivedData/Tour Manager/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FSCalendar.build/Objects-normal/arm64/FSCalendar-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/shredding/ALL\ MY/Programming/Swift/Tour\ Manager/DerivedData/Tour\ Manager/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FSCalendar.build/module.modulemap \
-  /Users/shredding/ALL\ MY/Programming/Swift/Tour\ Manager/Pods/Target\ Support\ Files/FSCalendar/FSCalendar-dummy.m \
-  /Users/shredding/ALL\ MY/Programming/Swift/Tour\ Manager/Pods/Target\ Support\ Files/FSCalendar/FSCalendar-prefix.pch
--- DerivedData/Tour Manager/Build/Intermediates.noindex/Tour Manager.build/Debug-iphonesimulator/Tour Manager.build/Script-E013215BF8D4058A5659F0E1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${PODS_ROOT}/Target Support Files/Pods-Tour Manager/Pods-Tour Manager-frameworks.sh"
-
