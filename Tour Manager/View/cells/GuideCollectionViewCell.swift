d6407a9e76efa4b18cdce72d5db4f8a437d37f1b
Egor Zavrazhnov <88378430+SHREDDING8@users.noreply.github.com>
2023-06-20 19:15:28 +0300

    updated guides cell for new tour

---
+++ Tour Manager/View/cells/GuideCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GuideCollectionViewCell.swift
+//  Tour Manager
+//
+//  Created by SHREDDING on 20.06.2023.
+//
+
+import UIKit
+
+class GuideCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
