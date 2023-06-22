41c8d586c29d816aa3199d7e5abf15f07740609a
Egor Zavrazhnov <88378430+SHREDDING8@users.noreply.github.com>
2023-06-22 16:48:55 +0300

    fixes

--- Tour Manager/Controller/Main/Excursions/Excursion for guide/ExcursionForGuideTableViewController.swift
+++ Tour Manager/Controller/Main/Excursions/Excursion for guide/ExcursionForGuideTableViewController.swift
@@ -38,11 +38,65 @@ class ExcursionForGuideTableViewController: UITableViewController {
     
     
     
+    let acceptUIView = {
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let path = UIBezierPath(roundedRect:view.bounds,
+                                byRoundingCorners:[.topRight, .topLeft],
+                                cornerRadii: CGSize(width: 20, height:  20))
+        let maskLayer = CAShapeLayer()
+
+        maskLayer.path = path.cgPath
+        view.layer.mask = maskLayer
+        
+        let label = UILabel()
+        label.text = "123123"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .blue
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureValues()
+    
+        self.navigationItem.title = excursion.excursionName
+        
+        self.navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    
+    fileprivate func configureView(){
+        self.view.superview!.addSubview(self.acceptUIView)
+        self.view.bringSubviewToFront(self.acceptUIView)
+        NSLayoutConstraint.activate([
+            self.acceptUIView.bottomAnchor.constraint(equalTo: self.view.superview!.safeAreaLayoutGuide.bottomAnchor),
+            self.acceptUIView.leadingAnchor.constraint(equalTo: self.view.superview!.safeAreaLayoutGuide.leadingAnchor),
+            self.acceptUIView.trailingAnchor.constraint(equalTo: self.view.superview!.safeAreaLayoutGuide.trailingAnchor),
+            self.acceptUIView.heightAnchor.constraint(equalToConstant: (self.view.superview!.frame.height) / 2)
+        ])
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        configureView()
+        self.view.bringSubviewToFront(self.acceptUIView)
+        acceptUIView.backgroundColor = .black
     }
     
     
